7c40d343c5f04bf0573c678de8b575bdea37e48b
ryan farber <32393930+ryanfarber@users.noreply.github.com>
2020-11-29 18:49:30 -0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # zalgo-generator
- generates zalgo text
+generates zalgo text
+
+
+
+```javascript
+var Zalgo = require("zalgo-generator");
+
+var zalgo = new Zalgo({
+	amount: "mini", // 1, 2, 3 or small, medium, large, etc 
+	up: true,
+	middle: true,
+	down: true,
+})
+
+console.log(zalgo.generate("hello"))
+
+
+// h̵̟̱̟̘͚̺̪ͤͩé̹̠̭͆͟l̩̰̬̰̒̃̎͘l̟̱̙̅ǫ̜̩ͫͦͭ̑
+
+
+
+```
---
+++ generator.js
@@ -0,0 +1,182 @@
+
+var { zalgo_up, zalgo_down, zalgo_mid } = require("./characters.js")
+			
+// rand funcs
+//---------------------------------------------------
+
+//gets an int between 0 and max
+function rand(max)
+{
+	return Math.floor(Math.random() * max);
+}
+
+//gets a random char from a zalgo char table
+function rand_zalgo(array)
+{
+	var ind = Math.floor(Math.random() * array.length);
+	return array[ind];
+}
+
+// utils funcs
+//---------------------------------------------------
+
+//hide show element
+function toggle(id)
+{
+	if(document.getElementById(id).style.display == "none")
+		document.getElementById(id).style.display = "block";
+	else
+		document.getElementById(id).style.display = "none";
+}
+
+//lookup char to know if its a zalgo char or not
+function is_zalgo_char(c)
+{
+	var i;
+	for(i=0; i<zalgo_up.length; i++)
+		if(c == zalgo_up[i])
+			return true;
+		for(i=0; i<zalgo_down.length; i++)
+			if(c == zalgo_down[i])
+				return true;
+			for(i=0; i<zalgo_mid.length; i++)
+				if(c == zalgo_mid[i])
+					return true;
+				return false;
+			}
+
+			function draw_zalgo_table(elid)
+			{
+				var container = document.getElementById(elid);
+				var html = '';
+
+				html += '<b>Chars going up:</b><br />\n';
+				html += '<table class="zalgo_ref_table">\n';
+				html += '<tr>\n';
+				for(var i=0; i<zalgo_up.length; i++)
+				{
+					if(!(i % 10))
+						html += '</tr><tr>';
+					html += '<td class="zalgo_td">' + zalgo_up[i] + '</td>\n';
+				}
+				html += '</tr>\n';
+				html += '</table>\n';
+
+				html += '<br /><b>Chars staying in the middle:</b><br />\n';
+				html += '<table class="zalgo_ref_table">\n';
+				html += '<tr>\n';
+				for(var i=0; i<zalgo_mid.length; i++)
+				{
+					if(!(i % 10))
+						html += '</tr><tr>';
+					html += '<td class="zalgo_td">' + zalgo_mid[i] + '</td>\n';
+				}
+				html += '</tr>\n';
+				html += '</table>\n';
+
+				html += '<br /><b>Chars going down:</b><br />\n';
+				html += '<table class="zalgo_ref_table">\n';
+				html += '<tr>\n';
+				for(var i=0; i<zalgo_down.length; i++)
+				{
+					if(!(i % 10))
+						html += '</tr><tr>';
+					html += '<td class="zalgo_td">' + zalgo_down[i] + '</td>\n';
+				}
+				html += '</tr>\n';
+				html += '</table>\n';
+
+				container.innerHTML = html;
+			}
+
+// main shit
+//---------------------------------------------------
+function Zalgo(settings) {
+	// var p = document.getElementById(id);
+	this.generate = function(text) {
+
+		var amountMap = new Map([
+			[1, 1],
+			[2, 2],
+			[3, 3],
+			["mini", 1],
+			["small", 1],
+			["min", 1],
+			["minimum", 1],
+			["normal", 2],
+			["medium", 2],
+			["default", 2],
+			["norm", 2],
+			["max", 3],
+			["maxi", 3],
+			["maximum", 3],
+			["large", 3]
+		])
+		var amount = amountMap.get(settings.amount)
+
+		var txt = text;
+		var newtxt = '';
+
+		for(var i=0; i<txt.length; i++)
+		{
+			if(is_zalgo_char(txt.substr(i, 1)))
+				continue;
+			
+			var num_up;
+			var num_mid;
+			var num_down;
+			
+			//add the normal character
+			newtxt += txt.substr(i, 1);
+
+			//options
+			if(amount == 1) {
+				num_up = rand(8);
+				num_mid = rand(2);
+				num_down = rand(8);
+			}
+			else if(amount == 2) {
+				num_up = rand(16) / 2 + 1;
+				num_mid = rand(6) / 2;
+				num_down = rand(16) / 2 + 1;
+			}
+			else if(amount == 3) {
+				num_up = rand(64) / 4 + 3;
+				num_mid = rand(16) / 4 + 1;
+				num_down = rand(64) / 4 + 3;
+			}
+			
+			
+			if(settings.up)
+				for(var j=0; j<num_up; j++)
+					newtxt += rand_zalgo(zalgo_up);
+				if(settings.middle)
+					for(var j=0; j<num_mid; j++)
+						newtxt += rand_zalgo(zalgo_mid);
+					if(settings.down)
+						for(var j=0; j<num_down; j++)
+							newtxt += rand_zalgo(zalgo_down);
+					}
+
+		//result is in nextxt, display that
+		return newtxt
+		// //remove all children of lulz_container
+		// var container = document.getElementById('lulz_container');
+		// while(container.childNodes.length)
+		// 	container.removeChild(container.childNodes[0]);
+
+		// //build blocks for each line & create a <br />
+		// var lines = newtxt.split("\n");
+		// for(var i=0; i<lines.length; i++)
+		// {
+		// 	var n = document.createElement('text');
+		// 	n.innerHTML = lines[i];
+		// 	container.appendChild(n);
+		// 	var nl = document.createElement('br');
+		// 	container.appendChild(nl);
+		// }
+	}
+	//done
+}
+
+module.exports = Zalgo
